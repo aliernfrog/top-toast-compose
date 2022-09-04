5ffa868c1979d2c8f2b9ce45c78ab284fb0adf8e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-09-04 16:17:15 +0300

    feat: transparent system bars for demo app

--- app/src/main/java/com/aliernfrog/toptoastdemo/MainActivity.kt
+++ app/src/main/java/com/aliernfrog/toptoastdemo/MainActivity.kt
@@ -4,10 +4,8 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
@@ -16,8 +14,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.aliernfrog.toptoast.TopToastBase
 import com.aliernfrog.toptoast.TopToastColorType
 import com.aliernfrog.toptoast.TopToastManager
@@ -28,6 +29,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         topToastManager = TopToastManager()
         setContent {
             TopToastComposeTheme {
@@ -42,6 +44,7 @@ class MainActivity : ComponentActivity() {
     private fun MainColumn() {
         val context = LocalContext.current
         Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()), horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(Modifier.fillMaxWidth().height(getStatusBarHeight()).background(Color.Black))
             Spacer(Modifier.height(100.dp))
             Button(content = { Text("Normal toast") }, onClick = { topToastManager.showToast("This is a normal toast") })
             Button(content = { Text("Toast with icon") }, onClick = {
@@ -55,6 +58,12 @@ class MainActivity : ComponentActivity() {
                     (context as Activity).finish()
                 })
             })
+            Spacer(Modifier.height(100.dp))
         }
+    }
+
+    @Composable
+    fun getStatusBarHeight(): Dp {
+        return WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
     }
 }
