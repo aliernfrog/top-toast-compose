8994a80f1a2438710fceed9d1c3573c052c14d24
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-10-09 19:44:08 +0300

    feat: make ripple respect theme

--- TopToast/src/main/java/com/aliernfrog/toptoast/TopToast.kt
+++ TopToast/src/main/java/com/aliernfrog/toptoast/TopToast.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -137,7 +140,7 @@ fun TopToastBase(
 @Composable
 fun TopToast(manager: TopToastManager) {
     var modifier = Modifier.shadow(10.dp, RoundedCornerShape(30.dp)).clip(RoundedCornerShape(30.dp)).background(MaterialTheme.colorScheme.surface)
-    if (manager.onClick != null) modifier = modifier.clickable { manager.onClick?.invoke() }
+    if (manager.onClick != null) modifier = modifier.clickable(interactionSource = remember { MutableInteractionSource() }, indication = rememberRipple(color = MaterialTheme.colorScheme.onBackground), onClick = manager.onClick!!)
     Column(Modifier.fillMaxWidth().padding(top = getStatusBarHeight()+8.dp).padding(start = 24.dp, end = 24.dp, bottom = 10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         Row(modifier.border(1.dp, MaterialTheme.colorScheme.onBackground.copy(0.1f), RoundedCornerShape(30.dp)).padding(16.dp).animateContentSize()) {
             if (manager.icon != null || manager.iconDrawableId != null) Image(
