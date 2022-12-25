a144e780ba2c5c8a4525900b33c9dd5a3ce7c5e7
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-12-25 16:06:44 +0300

    feat: adjust TopToast appearance

--- library/src/main/java/com/aliernfrog/toptoast/util/extensions/Modifier.kt
+++ library/src/main/java/com/aliernfrog/toptoast/util/extensions/Modifier.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.remember
@@ -23,7 +22,7 @@ import com.aliernfrog.toptoast.util.toastShape
  */
 fun Modifier.toastModifier(topToastState: TopToastState): Modifier = composed {
     var modifier = this
-        .shadow(10.dp, RoundedCornerShape(30.dp)).clip(RoundedCornerShape(30.dp))
+        .shadow(2.dp, toastShape).clip(toastShape)
         .background(MaterialTheme.colorScheme.surface)
     if (topToastState.onClick != null) modifier = modifier.clickable(
         interactionSource = remember { MutableInteractionSource() },
@@ -32,8 +31,8 @@ fun Modifier.toastModifier(topToastState: TopToastState): Modifier = composed {
     )
     modifier
         .border(
-            width = 1.dp,
-            color = MaterialTheme.colorScheme.onBackground.copy(0.1f),
+            width = 0.1.dp,
+            color = MaterialTheme.colorScheme.onSurface.copy(0.15f),
             shape = toastShape
         )
         .padding(16.dp)
