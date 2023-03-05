e4c3667686d9bc1d7db828a23e0eb46098216aa0
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-03-05 17:20:45 +0300

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("com.android.application") version "7.4.0" apply false
     id("com.android.library") version "7.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
 // Demo application removes all "."s from libraryVersion and converts it to a Int, then uses it as versionCode
 val libraryVersion by extra { "1.1.1" }
-val composeCompilerVersion by extra { "1.3.2" }
-val composeVersion by extra { "1.4.0-alpha03" }
-val material3Version by extra { "1.1.0-alpha04" }
+val composeCompilerVersion by extra { "1.4.3" }
+val composeVersion by extra { "1.4.0-beta02" }
+val material3Version by extra { "1.1.0-alpha07" }
