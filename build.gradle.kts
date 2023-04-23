b85400a909b287005e1f06e67d566f7891abfb87
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-23 11:46:11 +0300

    chore: bump library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
 
-val libraryVersionName by extra { "1.2.0" }
-val libraryVersionCode by extra { 120 }
+val libraryVersionName by extra { "1.2.1" }
+val libraryVersionCode by extra { 121 }
 
 val composeCompilerVersion by extra { "1.4.6" }
 val composeVersion by extra { "1.4.2" }
