f219245871268758a0ca710e90b974106b5b6546
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-02-04 23:13:47 +0300

    fix(demo): handle system bar colors

--- demo/src/main/java/com/aliernfrog/toptoastdemo/ui/theme/Theme.kt
+++ demo/src/main/java/com/aliernfrog/toptoastdemo/ui/theme/Theme.kt
@@ -1,19 +1,47 @@
 package com.aliernfrog.toptoastdemo.ui.theme
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
+val supportsMaterialYou = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+@SuppressLint("NewApi")
 @Composable
 fun TopToastComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val dynamicColors = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     val colors = when {
-        dynamicColors && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
-        dynamicColors && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        supportsMaterialYou && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        supportsMaterialYou && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
         darkTheme -> darkColorScheme()
         else -> lightColorScheme()
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) SideEffect {
+        val activity = view.context as Activity
+        val insetsController = WindowCompat.getInsetsController(activity.window, view)
+        val transparentColor = Color.Transparent.toArgb()
+
+        WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+
+        activity.window.statusBarColor = transparentColor
+        activity.window.navigationBarColor = transparentColor
+
+        if (Build.VERSION.SDK_INT >= 29) {
+            activity.window.isNavigationBarContrastEnforced = false
+        }
+
+        insetsController.isAppearanceLightStatusBars = !darkTheme
+        insetsController.isAppearanceLightNavigationBars = !darkTheme
     }
 
     MaterialTheme(
