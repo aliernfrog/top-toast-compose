171b973d55ef1057033741f2a69bcfcedb5ae5b0
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-02-05 21:51:46 +0300

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("com.android.library") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
@@ -9,5 +9,5 @@ val libraryVersionCode by extra { 140 }
 
 val coreVersion by extra { "1.12.0" }
 val composeCompilerVersion by extra { "1.5.8" }
-val composeVersion by extra { "1.6.0-rc01" }
-val material3Version by extra { "1.2.0-beta02" }
+val composeVersion by extra { "1.6.0" }
+val material3Version by extra { "1.2.0-rc01" }
